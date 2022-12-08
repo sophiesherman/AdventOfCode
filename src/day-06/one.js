e8a62e6299ff406f4e7295bd0d8a3336d1608ee2
Sophie <sophie.sherman@martinit.com.au>
2022-12-09 07:57:43 +1100

    Day 6

---
+++ src/day-06/one.js
@@ -0,0 +1,40 @@
+import { URL, fileURLToPath } from 'url'
+import { getInputInCorrectFormat } from '../helper/helperFunctions.js'
+import { readInput } from '../util/readInput.js'
+
+const getSolution = (data) => {
+  let unqiueMarker = null
+  for (let i = 3; i < data.length; i++) {
+    if (unqiueMarker === null) {
+      const marker = {}
+      for (let k = i - 3; k <= i; k++) {
+        const letter = data[k]
+        marker[`${letter}`] = (marker?.[`${letter}`] || 0) + 1
+      }
+
+      const duplicates = Object.values(marker).filter((value) => value > 1)
+      if (duplicates.length === 0) {
+        unqiueMarker = i + 1
+      }
+    }
+  }
+  return unqiueMarker
+}
+
+// SETUP
+const sampleInputPath = fileURLToPath(
+  new URL('./inputSample.txt', import.meta.url)
+)
+const inputPath = fileURLToPath(new URL('./input.txt', import.meta.url))
+const sampleData = await readInput(sampleInputPath)
+const data = await readInput(inputPath)
+
+if (sampleData) {
+  console.log('SAMPLE')
+  console.log(getSolution(sampleData))
+}
+
+if (data) {
+  console.log('ANSWER')
+  console.log(getSolution(data))
+}
